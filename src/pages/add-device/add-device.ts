d7f9eba20e3134ff599f8f9b8a9c6c44832b7457
user-PC\user <mohamed.abozyd@gmail.com>
2018-05-04 22:38:21 +1200

    Test getCurrentSSID for Android

--- src/pages/add-device/add-device.ts
+++ src/pages/add-device/add-device.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 declare let cordova: any;
 
@@ -8,13 +9,28 @@ declare let cordova: any;
   templateUrl: 'add-device.html',
 })
 export class AddDevicePage {
+  currentSSD: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public alertCtrl: AlertController
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddDevicePage');
     console.log(cordova);
+    cordova.WifiWizard.getCurrentSSID((ssd: string)=>{
+      let alert = this.alertCtrl.create({
+        title: 'SSID Found!',
+        subTitle: `Your SSID is ${ssd}`,
+        buttons: ['OK']
+      });
+      alert.present();
+    }, ()=>{
+
+    });
   }
 
 }
