a4dd8094c65060280ced97f4529f78d4e7f4c533
user-PC\user <mohamed.abozyd@gmail.com>
2018-05-05 10:20:42 +1200

    Wifiwizerd test 1

--- src/pages/add-device/add-device.ts
+++ src/pages/add-device/add-device.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 
-declare let cordova: any;
+declare var WifiWizard: any;
 
 @Component({
   selector: 'page-add-device',
@@ -20,17 +20,42 @@ export class AddDevicePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddDevicePage');
-    console.log(cordova);
-    cordova.WifiWizard.getCurrentSSID((ssd: string)=>{
-      let alert = this.alertCtrl.create({
-        title: 'SSID Found!',
-        subTitle: `Your SSID is ${ssd}`,
-        buttons: ['OK']
-      });
-      alert.present();
-    }, ()=>{
+    //console.log(cordova);
+    this.showAlert('Before check ssd', 'we are working');
+    this.getSsidName();
+    /* if(cordova){
+      cordova.WifiWizard.getCurrentSSID((ssd: string)=>{
+        this.showAlert('SSID Found!', `Your SSID is ${ssd}`);
+      }, ()=>{});
+    }else{
+      this.showAlert('cordova is null', 'please check what is wrong');
+    } */
+    
+   // this.showAlert('After check ssd', 'we are done checking');
+  }
 
+  showAlert(title: string, subTitle: string){
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: subTitle,
+      buttons: ['OK']
     });
+    alert.present();
+  }
+  errorHandler(err: any) {
+    alert(`Problem: ${err}`);
+  }
+
+  getSsidName() {
+    WifiWizard.getCurrentSSID((ssid: string) => alert(`Your SSID: ${ssid}`), this.errorHandler);
+  }
+
+  isWifiEnabled() {
+    WifiWizard.isWifiEnabled(truthy => alert(`Wifi Enabled: ${truthy}`), this.errorHandler);
+  }
+
+  listNetworks() {
+    WifiWizard.listNetworks(networks => alert(`Networks: ${networks}`), this.errorHandler);
   }
 
 }
